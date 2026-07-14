842a87fc9ecc2964d4b0f623c3a8283925d25155
govergu <kritishsainju2063@gmail.com>
2026-07-14 20:36:34 +0545

    chore: logger

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import { ENV } from "@config/env.js";
 import app from "./app.js";
 import { connectDB } from "@infrastructure/db.js";
+import { logger } from "@lib/logger.js";
 
 const startServer = async () => {
   await connectDB();
 
   app.listen(ENV.PORT, () => {
-    console.log(`Server is running at port: ${ENV.PORT}`);
+    logger.info(`Server is running at port: ${ENV.PORT}`);
   });
 };
 

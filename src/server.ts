f14e327a80e950adbb79e65083dbcf00ea4cca35
govergu <kritishsainju2063@gmail.com>
2026-07-14 20:48:22 +0545

    feat: graceful shutdown

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,16 @@ import { ENV } from "@config/env.js";
 import app from "./app.js";
 import { connectDB } from "@infrastructure/db.js";
 import { logger } from "@lib/logger.js";
+import { setupGracefulShutdown } from "@config/shutdown.js";
 
 const startServer = async () => {
   await connectDB();
 
-  app.listen(ENV.PORT, () => {
+  const server = app.listen(ENV.PORT, () => {
     logger.info(`Server is running at port: ${ENV.PORT}`);
   });
+
+  setupGracefulShutdown(server);
 };
 
 startServer();

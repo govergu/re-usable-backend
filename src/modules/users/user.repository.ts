c9fac5b38b03f015496196a3b8595f798fc8fd08
govergu <kritishsainju2063@gmail.com>
2026-07-20 16:06:46 +0545

    feat: query builder inside utils

--- src/modules/users/user.repository.ts
+++ src/modules/users/user.repository.ts
@@ -16,4 +16,27 @@ export class UserRepository extends BaseRepository<
       where: { email },
     });
   }
+  // demo use purpose of query builders
+  //   async function getAllUsers(query: QueryParams) {
+  //   const { page, limit, skip } = QueryBuilder.getPagination(query);
+  //   const orderBy = QueryBuilder.getSort(query);
+
+  //   // Extract custom dynamic filtering tags (excluding structural keywords)
+  //   const { page: _, limit: __, sort: ___, order: ____, search, ...filters } = query;
+
+  //   const whereConditions = {
+  //     ...filters, // E.g., automatically passes { role: 'admin' } straight to Prisma
+  //     ...(search && { name: { contains: search, mode: 'insensitive' } })
+  //   };
+
+  //   const [data, total] = await prisma.$transaction([
+  //     prisma.user.findMany({ skip, take: limit, orderBy, where: whereConditions }),
+  //     prisma.user.count({ where: whereConditions })
+  //   ]);
+
+  //   return {
+  //     data,
+  //     meta: QueryBuilder.buildMeta(page, limit, total)
+  //   };
+  // }
 }
